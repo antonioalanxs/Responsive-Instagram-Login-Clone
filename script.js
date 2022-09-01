1751f70ba024500e4fffbbb779ff6fe6f2ba6903
Antonio José Alanís Bernal <79718376+antonioalanxs@users.noreply.github.com>
2022-09-01 16:37:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const LOGO = document.querySelector("#logo");
+  const GITHUB = document.querySelector("#github-icon");
+  
+  LOGO.onclick = () => window.open("https://github.com/antonioalanxs/Responsive-Instagram-Login-Clone");
+  GITHUB.onclick = () => window.open("https://github.com/antonioalanxs");
+});
